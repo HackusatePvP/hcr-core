52a19cf62c7a250e1363ff714f1633ccd0aadb86
Hackusate_PvP <Supermftw@gmail.com>
2023-01-23 07:03:48 -0600

    Added dependency files.

---
+++ local/load.bat
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=files/assemble-1.0-SNAPSHOT.jar -DpomFile=files/assemble-1.0-SNAPSHOT.pom
+mvn install:install-file -Dfile=files/bukkitapi.jar -DpomFile=files/bukkitapi.pom
+mvn install:install-file -Dfile=files/bukkitapi-nethandler.jar -DpomFile=files/bukkitapi-nethandler.pom
